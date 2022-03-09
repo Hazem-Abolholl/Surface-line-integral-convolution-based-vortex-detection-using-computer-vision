b8e32f2b53140b1e296b347e2b422fd1bbbff7d2
Hazem Abolholl <57419359+Hazem-Abolholl@users.noreply.github.com>
2022-03-09 10:33:08 +0000

    Update Configration.py

--- VortexDetection/Configration.py
+++ VortexDetection/Configration.py
@@ -5,7 +5,7 @@ ANCHORS = [
     [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],[(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
     [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
 ]  # these have been Normalized to be between [0, 1]
-CHECKPOINT = "Weight/weights.pth.tar"
+CHECKPOINT = "weights.pth.tar"
 R = [Image_Size // 32, Image_Size // 16, Image_Size // 8]
 ###################################################################################################
 ####################architecture config according to the YOLOv3 paper:#############################
